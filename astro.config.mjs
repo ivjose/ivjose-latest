9ef44853b8354a3f64fabbeefec1a36d6af7ceb5
Jose IV <ivjosesantos@gmail.com>
2023-07-16 00:31:46 +0800

    Added hero and services cms data

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import tailwind from '@astrojs/tailwind';
+import storyblok from '@storyblok/astro';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 import { astroImageTools } from 'astro-imagetools';
 import { defineConfig } from 'astro/config';
+import { loadEnv } from 'vite';
+const { SECRET_STORYBLOK } = loadEnv(import.meta.env.MODE, process.cwd(), '');
 
-// https://astro.build/config
-
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [
@@ -14,5 +15,25 @@ export default defineConfig({
       // Useful if you need to define and/or import your own custom `base.css`.
       // config: { applyBaseStyles: false },
     }),
+    storyblok({
+      accessToken: SECRET_STORYBLOK,
+      apiOptions: {
+        region: 'eu',
+      },
+      components: {
+        page: 'storyblok/Page',
+        feature: 'storyblok/Feature',
+        grid: 'storyblok/Grid',
+        teaser: 'storyblok/Teaser',
+        hero: 'storyblok/Hero',
+        services: 'storyblok/Services',
+      },
+    }),
   ],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
 });
