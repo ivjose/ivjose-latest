21598addf8d776afb2fd1b7b56f7626d1792484c
Jose IV <ivjosesantos@gmail.com>
2025-01-23 09:50:26 +0800

    remove middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,12 @@
-import { NextRequest, NextResponse } from "next/server";
+// import { NextRequest, NextResponse } from "next/server";
 
-import { baseUrl } from "@/app/sitemap";
+// import { baseUrl } from "@/app/sitemap";
 
-export function middleware(request: NextRequest) {
-  if (
-    request.nextUrl.origin !== baseUrl &&
-    process.env.NODE_ENV !== "development"
-  ) {
-    return NextResponse.redirect(baseUrl);
-  }
-}
+// export function middleware(request: NextRequest) {
+//   if (
+//     request.nextUrl.origin !== baseUrl &&
+//     process.env.NODE_ENV !== "development"
+//   ) {
+//     return NextResponse.redirect(baseUrl);
+//   }
+// }
