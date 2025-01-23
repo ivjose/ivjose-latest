b60acf455d144e940f4065eaac95b1127f1440ef
Jose IV <ivjosesantos@gmail.com>
2025-01-23 11:25:44 +0800

    Added middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,8 +3,13 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export default function myMiddleware(request: NextRequest) {
-  // Your Middleware logic here
-  console.log(request);
+    const MY_URL = 'https://ivjose.com'
+    if (
+      request.nextUrl.origin !== MY_URL &&
+      process.env.NODE_ENV !== "development"
+    ) {
+      return NextResponse.redirect(MY_URL);
+    } 
   
   return NextResponse.next(); // Pass control to the next Middleware or route handler
 }
