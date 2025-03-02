e9ee5994fb8bd0b260dcb859a91169ae80af9e1a
Jose IV <ivjosesantos@gmail.com>
2025-03-02 16:39:09 +0800

    Fixed redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,12 @@
  
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-
+import { baseUrl } from "./app/sitemap";
 
 export function middleware(request: NextRequest) { 
-    // console.log(request, "DDD");
-    
-  if (request.nextUrl.host === 'localhost:3000' || request.nextUrl.host === 'ivjose.com' ) {
+  if (request.nextUrl.origin === 'http://localhost:3001' || request.nextUrl.origin === 'https://ivjose.com' ) {
     return NextResponse.next()
   }
-  return NextResponse.redirect('/home')
+  return NextResponse.redirect(baseUrl)
 
 }
