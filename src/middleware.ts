23835cda08fe8fe8735b9d60439df6b8829dbb9e
Jose IV <ivjosesantos@gmail.com>
2025-03-02 15:53:20 +0800

    Updated middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ import type { NextRequest } from 'next/server'
 import { baseUrl } from "./app/sitemap";
 
 export function middleware(request: NextRequest) { 
-  if (request.nextUrl.hostname === 'localhost' || request.nextUrl.hostname === 'ivjose.com' ) {
+  if (request.nextUrl.host === 'localhost:3000' || request.nextUrl.host === 'ivjose.com' ) {
     return NextResponse.next()
   }
   return NextResponse.redirect(baseUrl)
